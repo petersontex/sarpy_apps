9c8e5bc993030b0c0179955331e8ba8b9ed8d262
Peterson <529965@bah.com>
2025-05-01 14:02:24 -0400

    Updated MITM and RCS package names and directories.

--- sarpy_apps/apps/RCS/src/setup.py
+++ sarpy_apps/apps/RCS/src/setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-    name="python-rcs",
-    version="1.0",
+    name="rcspy",
+    version="1.0.0",
     description="Python Radar Cross Section Tool",
     author="N-ASK Incorporated",
     author_email="parkison-alex@n-ask.com",
