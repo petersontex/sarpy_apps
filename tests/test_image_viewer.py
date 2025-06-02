837bfe29e10eade1fd36d9f08163a01d9a471a48
Peterson <529965@bah.com>
2025-06-02 07:56:42 -0400

    Fixing mock 01.

--- tests/test_image_viewer.py
+++ tests/test_image_viewer.py
@@ -1,15 +1,19 @@
 import pytest
-from sarpy_apps.apps.image_viewer import ImageViewer
+import os
+from sarpy_apps.apps.image_viewer import ImageViewer, AppVariables
 from tests import unittest
 from tk_builder.widgets.basic_widgets import Frame
 from tk_builder.image_reader import CanvasImageReader
+from tk_builder.base_elements import TypedDescriptor
 from sarpy.io.complex.base import SICDTypeReader
+from sarpy.io.general.base import BaseReader
 from tkinter import ttk
 import tkinter as tk
 from unittest.mock import MagicMock
 from unittest.mock import patch
 import unittest.mock as mock
 from sarpy_apps.supporting_classes.image_reader import SICDTypeCanvasImageReader
+from sarpy.io.general.base import BaseReader
 
 def get_menu_items(app):
     items = []
@@ -24,31 +28,32 @@ def get_menu_items(app):
 class TestImageViewerClass(unittest.TestCase):
     def setUp(self):
         root = tk.Tk()
-        self.app = ImageViewer(root, None)
+        # reader = SICDTypeCanvasImageReader(SICDTypeReader(BaseReader()))
+        the_style = ttk.Style()
+        the_style.theme_use('classic')
+        # self.app = ImageViewer(root, reader)
+        self.app = ImageViewer(root)
+        root.geometry("1000x800")
+
+    def tearDown(self):
+        self.app = None
 
     def test_default_title(self):
         self.assertEqual(self.app.winfo_toplevel().title(), "Image Viewer")
 
-    def test_single_filename_title(self):
-        with mock.patch.object(self.app.set_title, 'file_name') as mock_file_name: 
-            mock_file_name = "TestReader.nitf"
-            self.app.set_title()
-            self.assertEqual(self.app.winfo_toplevel().title(), 'Image Viewer for TestReader.nitf')
+    @patch.object(AppVariables, 'image_reader', return_value=True)
+    @patch.object(AppVariables.image_reader, 'file_name', create=True, return_value="/home/fakedir/TestReader.nitf")
+    def test_single_filename_title(self, mockAppVariables, mockCanvasImageReader):
+        self.app.variables.image_reader = mockAppVariables
+        self.app.set_title()
+        self.assertEqual(self.app.winfo_toplevel().title(), 'Image Viewer for TestReader.nitf')
 
-    def test_multiple_filenames_title(self):
-        with mock.patch("sarpy_apps.apps.image_viewer.AppVariables.image_reader") as reader:
-            self.app.set_title()
-            self.assertEqual(self.app.winfo_toplevel().title(), 'Image Viewer, Multiple Files')
-        
-    # def test_menu(self):
-    #     root = tk.Tk()
-    #     app = ImageViewer(root, None)
-    #     menu_items = get_menu_items(app)
-    #     assert "Open Image" in menu_items
+    @patch.object(AppVariables, 'image_reader', return_value=True)
+    @patch.object(AppVariables.image_reader, 'file_name', create=True, return_value=['/home/fakedir/TestReader.nitf', '/home/fakedir/TestReader2.nitf'])
+    def test_multiple_filename_title(self, mockAppVariables, mockImageReader):
+        # self.app.variables.image_reader = mockImageReader
+        self.app.set_title()
+        self.assertEqual(self.app.winfo_toplevel().title(), 'Image Viewer, Multiple Files')
 
-    def test_exit(self):
-        self.app.exit()
-        try:
-            assert self.app.winfo_exists() == False
-        except tk.TclError:
-            assert True
+    def test_update_reader(self):
+        self.app.update_reader(None)
